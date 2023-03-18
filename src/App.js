560d44cb78ec7e0e323e1b62f89a624d877f077b
Erum640 <101186928+Erum640@users.noreply.github.com>
2023-03-18 15:33:54 +0530

    signin and signup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
 import Landing from './routes/landing';
-
+import { Routes, Route } from 'react-router-dom';
+import SignIn from './routes/signin';
+import SignUp from './routes/signup';
+import Navbar from './components/navbar/navbar';
 const  App=()=> {
   return (
     <>
-    <Landing/>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Landing/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+    </Routes>
     </>
   );
 }
--- src/routes/landing.jsx
+++ src/routes/landing.jsx
@@ -3,8 +3,12 @@ import Navbar from "../components/navbar/navbar";
 const Landing = ()=>{
     return(
         <>
-        <Navbar/>
-
+        {/* <Navbar/> */}
+        <div className="flex flex-col items-center mx-12">
+        <div className="text-3xl font-semibold mt-28  text-center leading-snug text-gray-700">Your <span className="text-[#cc29ff] text-4xl">Health Vault</span> is Ready.</div>
+        <div className="text-3xl mt-9 text-center leading-snug ">A Next Gen Digital Health Record Platform</div>
+        <button className="text-2xl font-semibold rounded-[2rem] border-2 px-8 py-3 mt-12 border-black ">Sign In</button>
+        </div>
         </>
     )
 }
