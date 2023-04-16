24a67c1c8db3415ffe712e4d332538b6ec28715f
Samudra <samudramitra2017@gmail.com>
2023-04-16 10:24:59 +0530

    double kwotes

--- src/routes/previousinfo.jsx
+++ src/routes/previousinfo.jsx
@@ -1,35 +1,35 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import { Button } from '@mui/material';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import ImageUploader from '../components/imageUpload';
-import { useState } from 'react';
-import axios from 'axios';
-import { SentimentDissatisfied } from '@mui/icons-material';
-const BACKEND_URL = 'http://34.131.196.228/api/addImageToDB';
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import ImageUploader from "../components/imageUpload";
+import { useState } from "react";
+import axios from "axios";
+import { SentimentDissatisfied } from "@mui/icons-material";
+const BACKEND_URL = "http://34.131.196.228/api/addImageToDB";
 
 export default function Previousinfo() {
   const [data, setData] = useState({
     image_url: "",
     category: "",
-    dd: '',
-    mm: '',
-    yy: ''
+    dd: "",
+    mm: "",
+    yy: "",
   });
   const handleChange = async (e) => {
     const { name, value } = e.target;
     setData({ ...data, [name]: value });
     console.log({ ...data, [name]: value });
-  }
+  };
 
   return (
     <>
-      <Paper elevation={10} className='mt-28 w-1/2 mx-auto py-8 px-12'>
-        <div className='mb-8  text-3xl font-semibold'>
+      <Paper elevation={10} className="mt-28 w-1/2 mx-auto py-8 px-12">
+        <div className="mb-8  text-3xl font-semibold">
           Patient Previous Record
         </div>
         <Grid container spacing={4}>
@@ -43,11 +43,15 @@ export default function Previousinfo() {
                 onChange={(newValue) => {
                   let dateStamp = new Date(newValue);
                   console.log(dateStamp.getFullYear());
-                  setData({ ...data, dd: dateStamp.getDate(), mm: dateStamp.getMonth(), yy: dateStamp.getFullYear() })
+                  setData({
+                    ...data,
+                    dd: dateStamp.getDate(),
+                    mm: dateStamp.getMonth(),
+                    yy: dateStamp.getFullYear(),
+                  });
                 }}
               />
             </LocalizationProvider>
-
           </Grid>
           <Grid item xs={12} sm={6}>
             <TextField
@@ -64,16 +68,24 @@ export default function Previousinfo() {
               }}
             />
           </Grid>
-          <Button variant='contained' sx={{ marginTop: '10px', marginX: 'auto' }} onClick={async (e) => {
-            e.preventDefault();
-            // console.log(data);
-            const email = JSON.parse(localStorage['data']).data.email;
-            const res = await axios.post(BACKEND_URL, { ...data, email: email });
-            // console.log({...data,email:email});
-          }}>SUBMIT</Button>
+          <Button
+            variant="contained"
+            sx={{ marginTop: "10px", marginX: "auto" }}
+            onClick={async (e) => {
+              e.preventDefault();
+              // console.log(data);
+              const email = JSON.parse(localStorage["data"]).data.email;
+              const res = await axios.post(BACKEND_URL, {
+                ...data,
+                email: email,
+              });
+              // console.log({...data,email:email});
+            }}
+          >
+            SUBMIT
+          </Button>
         </Grid>
       </Paper>
-
     </>
   );
 }
