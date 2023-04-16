e9e931520c5805b5b6cf346ced57db186b6d25f5
Saumya Subhra Nandi <nandi.saumya2004@gmail.com>
2023-04-16 11:01:38 +0530

    asd

--- src/routes/previousinfo.jsx
+++ src/routes/previousinfo.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import { Button } from '@mui/material';
@@ -14,21 +13,6 @@ import { SentimentDissatisfied } from '@mui/icons-material';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 const BACKEND_URL = 'http://34.131.196.228/api/addImageToDB';
-=======
-import * as React from "react";
-import Grid from "@mui/material/Grid";
-import { Button } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import Paper from "@mui/material/Paper";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import ImageUploader from "../components/imageUpload";
-import { useState } from "react";
-import axios from "axios";
-import { SentimentDissatisfied } from "@mui/icons-material";
-const BACKEND_URL = "http://34.131.196.228/api/addImageToDB";
->>>>>>> 24a67c1c8db3415ffe712e4d332538b6ec28715f
 
 export default function Previousinfo() {
   const [data, setData] = useState({
@@ -46,7 +30,6 @@ export default function Previousinfo() {
 
   return (
     <>
-<<<<<<< HEAD
     <ToastContainer />
     <Paper elevation={10} className='mt-28 w-1/2 mx-auto py-8 px-12'>
       <div className='mb-8  text-3xl font-semibold'>
@@ -54,14 +37,6 @@ export default function Previousinfo() {
       </div>
       <Grid container spacing={4}>
           <ImageUploader setData={setData} data={data}/>
-=======
-      <Paper elevation={10} className="mt-28 w-1/2 mx-auto py-8 px-12">
-        <div className="mb-8  text-3xl font-semibold">
-          Patient Previous Record
-        </div>
-        <Grid container spacing={4}>
-          <ImageUploader setData={setData} data={data} />
->>>>>>> 24a67c1c8db3415ffe712e4d332538b6ec28715f
 
           <Grid item xs={12} sm={6}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -113,7 +88,7 @@ export default function Previousinfo() {
             SUBMIT
           </Button>
         </Grid>
-<<<<<<< HEAD
+        <Grid>
     <Button variant='contained' sx={{marginTop:'10px', marginX:'auto'}} onClick={async(e)=>{
       e.preventDefault();
       // console.log(data);
@@ -125,9 +100,6 @@ export default function Previousinfo() {
       </Grid>
     </Paper>
    
-=======
-      </Paper>
->>>>>>> 24a67c1c8db3415ffe712e4d332538b6ec28715f
     </>
   );
 }
